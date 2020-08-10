a2398ee8c1cbf4e1e224be540749f0763e3b5358
nada <nada.baklouti@esprit.tn>
2020-08-10 18:07:27 +0100

    React Router

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import { DISHES } from './shared/dishes';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 
 
 class App extends Component {
@@ -16,10 +17,11 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
