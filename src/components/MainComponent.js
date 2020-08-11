29313f2944f3c2e572e7243e39f27a0ce54d74ce
nada <nada.baklouti@esprit.tn>
2020-08-11 20:48:17 +0100

    Controlled Forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -11,6 +11,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
+import About from './AboutComponent';
 
 class Main extends Component {
 
@@ -53,6 +54,7 @@ class Main extends Component {
         <Switch>
           <Route path='/home' component={HomePage} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
           <Route exact path='/contactus' component={Contact} />} />
           <Route path='/menu/:dishId' component={DishWithId} />
           <Redirect to="/home" />
