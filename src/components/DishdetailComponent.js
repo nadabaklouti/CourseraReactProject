a1be74f36a09219f20d23bc36216ae09feb7d527
nada <nada.baklouti@esprit.tn>
2020-08-17 17:37:35 +0100

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -6,15 +6,11 @@ import {
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
-
-
+import { baseUrl } from '../shared/baseUrl';
 
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
-
-
-
 
 
 class CommentForm extends Component {
@@ -112,7 +108,7 @@ function RenderDish({ dish }) {
 
     return (
         <Card>
-            <CardImg top src={dish.image} alt={dish.name} />
+            <CardImg top src={baseUrl + dish.image} alt={dish.name} />
             <CardBody>
                 <CardTitle>{dish.name}</CardTitle>
                 <CardText>{dish.description}</CardText>
